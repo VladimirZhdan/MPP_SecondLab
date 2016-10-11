d6a44a324ce12ac2507497a3d22044f8928f93d2
Vladimir Zhdan <zhdan-vladimir@mail.ru>
2016-10-11 18:50:53 +0300

    ADD events in SourceObject

--- MPP_WeakDelegate/SourceObject.cs
+++ MPP_WeakDelegate/SourceObject.cs
@@ -8,5 +8,11 @@ namespace MPP_WeakDelegate
 {
     class SourceObject
     {
+        public event Action<int> Completed;
+        public event Action<int, double> Completed1;
+        public event Action<int, double, int> Completed2;
+        public event Action<int, int, int, int> Completed3;
+
+
     }
 }
