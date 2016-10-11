17af7bd0bdcdac8aeacc11d3cbe9356e7176f7b5
Vladimir Zhdan <zhdan-vladimir@mail.ru>
2016-10-11 18:44:58 +0300

    Initial commit

---
+++ MPP_WeakDelegate.Tests/MemoryTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MPP_WeakDelegate.Tests
+{
+    [TestClass]
+    class MemoryTest
+    {
+        [TestMethod]
+        public void CheckMemory()
+        {
+
+        }
+    }
+}
